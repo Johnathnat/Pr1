90988069da13d9ba69680cca59363d904ba26aac
Johnathnat <22225356@bucks.ac.uk>
2022-12-14 04:31:37 +0000

    I was messing around in testing it and i accidently got it to kinda function its definatly not done though.

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,54 @@ import java.util.Scanner;
  * beans
  */
 public class Main {
+    /**
+     * Variables
+     * Hero Item (sword and shield)
+     * Neutral Item (long sword)
+     * Evil Item (dark dagger)
+     * WTF item (lighter)
+     *
+     * @return
+     */
+    static boolean SwordShield(boolean hero)
+    {
+        //hero=false;
+        return hero;
+    }
+    static boolean LongSword(boolean neutral)
+    {
+        //neutral=false;
+        return neutral;
+    }
+    static boolean DarkDagger(boolean evil)
+    {
+        //evil=false;
+        return evil;
+    }
+    static boolean lighter(boolean WTF)
+    {
+        //WTF=false;
+        return WTF;
+    }
+
+    static boolean vitality (boolean potion)
+    {
+        //potion=false;
+        return potion;
+    }
+
     public static void main(String[] args) {
         bulkOfGame();
     }
-
+    static void intro()
+    {
+        System.out.println(" WELCOME TO EN PISEE'S EXCELLENT ADVENTURE!");
+        System.out.println
+                (
+                        "My name in Enpisee i will be the narrator of this adventure.\n" +
+                                "Now lets Begin..."
+                );
+    }
     static void bulkOfGame()
     {
 
@@ -17,82 +61,189 @@ public class Main {
         house();
         System.out.println
                 (
-                        "\n You are in a creepy house! Would you like to:" +
-                                "\n1) Go into the kitchen? " +
-                                "\n2) Leave?"
+                        "\nNPC welcome" +
+                        "\n1) Progress" +
+                        "\n2) Insult"
                 );
         System.out.print("> ");
-        //Fist input going up or down
-        int Go;
-        Go = Integer.parseInt(keyboard.next());
-        switch (Go) {
+
+        int welcome = Integer.parseInt(keyboard.next());
+        switch (welcome)
+        {
+            //progress
             case 1:
+                System.out.println("Progress");
+            break;
+
+            //Go option 2
+            case 2:
+                System.out.println("Insulted");
+            break;
+        }
+
+        //Village
+        int i = 0;
+        while (i < 5)
+        {
+            System.out.println
+                    (
+                            "En Pisee introduces the village and their story" +
+                                    "\n1) insult " +
+                                    "\n2) pity villagers " +
+                                    "\n3) Sympathise with villagers" +
+                                    "\n4) Look at them in disgust"
+                    );
+            System.out.print(">  ");
+            int village = Integer.parseInt(keyboard.next());
+            switch (village)
+            {
+                //village option: Insult
+                case 1:
+                    System.out.println("Insult");
+                    i = i+1;
+                    //game over
+                    break;
+                //village option: Pity
+                case 2:
+                    i=6;
+                    System.out.println("Pity");
+
+                    break;
+                //village option: Sympathise
+                case 3:
+                    System.out.println("Sympathise");
+                    i=6;
+                    break;
+
+                //village option: disgust
+                case 4:
+                    System.out.println("Disgust");
+                    i=6;
+                    break;
+            }
+        }
+        if (i == 5)
+        {
+            //NPC leaves ending
+            System.out.println("NPC leaves");
+            gameOver();
+        }
+
+
+        //Choose your weapon
+        else
+        {
+            System.out.println
+                    (
+                            "En Pisee sets player up to pick their items" +
+                                    "\n1) Sword and shield " +
+                                    "\n2) Long Sword " +
+                                    "\n3) Dark Dagger" +
+                                    "\n4) Lighter"
+                    );
+            System.out.print(">  ");
+            int weapon = Integer.parseInt(keyboard.next());
+            switch (weapon)
+            {
+                //weapon option: Sword and shield
+                case 1:
+                    System.out.println("Sword and shield");
+                    boolean hero=SwordShield(true);
+
+                    break;
+                //weapon option: Long Sword
+                case 2:
+                    System.out.println("Long Sword");
+                    LongSword(true);
+
+                    break;
+                //weapon option: Dark Dagger
+                case 3:
+                    System.out.println("Dark Dagger");
+                    DarkDagger(true);
+                    break;
+
+                //weapon option: Lighter
+                case 4:
+                    System.out.println("Lighter");
+                    lighter(true);
+                    break;
+            }
+
+            //Old Crone
+            boolean hag = false;
+            while (hag==false)
+            {
                 System.out.println
                         (
-                                "There is a long countertop with dirty dishes everywhere. Off to one side there is, as you'd expect, a refrigerator. You may: " +
-                                        "\n1) Open the refrigerator " +
-                                        "\n2) Look in the pantry. "
+                                "An old crone. Offering aid to the player." +
+                                        "\n1) Accept" +
+                                        "\n2) Ignore" +
+                                        "\n3) Berate"
                         );
                 System.out.print(">  ");
-                int Look = Integer.parseInt(keyboard.next());
-                switch (Look) {
-                    //Look option 1
+                int crone = Integer.parseInt(keyboard.next());
+                switch (crone)
+                {
+                    //crone option: Accept
                     case 1:
-                        System.out.println
-                                (
-                                        "Inside the refrigerator you see food and stuff. It looks pretty nasty. Would you like to: " +
-                                                "\n1)Eat some of the food?" +
-                                                "\n2)Leave it?"
-                                );
-                        System.out.print(">  ");
-                        int Eat = Integer.parseInt(keyboard.next());
-                        switch (Eat) {
-                            //Eat option 1
-                            case 1:
-                                System.out.println("You live!");
-                                gameOver();
-                                break;
-                            //Eat option 2
-                            case 2:
-                                System.out.println("You die of hunger.");
-                                gameOver();
-                                break;
+                        System.out.println("Accept");
+                        if (hero==true)
+                        {
+                            vitality(true);
+                            System.out.println("you're given a potion and feel great");
                         }
+                        else if (netral==true)
+                        {
+                            System.out.println("you're given a potion and nothing happens");
+                        }
+                        else
+                        {
+                            System.out.println("nothing happens");
+                        }
+                        hag=true;
                         break;
-                    //Look option 2
+                    //crone option: Ignore
                     case 2:
-                        System.out.println
-                                (
-                                        "There is a killer inside. Do you want to: " +
-                                                "\n1) Fight them?" +
-                                                "\n2) Run away?"
-                                );
+                        System.out.println("Ignore");
+                        hag=true;
+                        break;
+                    //crone option: Berate
+                    case 3:
+                        System.out.println("Berate");
+                        System.out.println("you become a cactus");
+                        System.out.println("1) Restart" +
+                                "\n2) End Game");
                         System.out.print(">  ");
-                        int Pantry = Integer.parseInt(keyboard.next());
-                        switch (Pantry) {
-                            //Pantry option 1
-                            case 1:
-                                System.out.println("\nYou're weak and die");
-                                gameOver();
-                                break;
-                            //Pantry option 2
-                            case 2:
-                                System.out.println("\nYou died because your too slow & can't run");
-                                gameOver();
-                                break;
+                        int cactus = Integer.parseInt(keyboard.next());
+                        if (cactus==2)
+                        {
+                            gameOver();
+                            hag=true;
+                            break;
                         }
                         break;
                 }
-                break;
-            //Go option 2
-            case 2:
-                System.out.println("You leave");
-                gameOver();
-                break;
+            }
+
+
         }
     }
+    static void gameOver()
+    {
+        System.out.println("<     Game Over     >");
+        System.out.println("<      Made By      >");
+        System.out.println("<  Johnathan & Ana  >");
 
+    }
+    //player
+    static void player()
+    {
+    }
 
+    //paths
+
+    //Ascii Art
     static void house()
     {
         System.out.println("" +
@@ -103,21 +254,4 @@ public class Main {
                 "|_|_|I|_|_|");
     }
 
-    static void intro()
-    {
-        System.out.println(" WELCOME TO A TINY ADVENTURE");
-        System.out.println
-                (
-                        "My name in NPC i will be the narrator of this adventure.\n" +
-                                "Now lets Begin..."
-                );
-    }
-
-    static void gameOver()
-    {
-        System.out.println("<  Game Over  >");
-        System.out.println("<   Made By   >");
-        System.out.println("<  Johnathan  >");
-
-    }
 }
